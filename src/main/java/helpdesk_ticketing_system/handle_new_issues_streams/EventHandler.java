bb16527dad6bc26c56e1d0802a4711ea4605f4bb
sayantan <sayantandas657@outlook.com>
2023-04-24 12:04:30 +0530

    Added code to save new issue to mongo db

--- src/main/java/helpdesk_ticketing_system/handle_new_issues_streams/EventHandler.java
+++ src/main/java/helpdesk_ticketing_system/handle_new_issues_streams/EventHandler.java
@@ -3,21 +3,23 @@ package helpdesk_ticketing_system.handle_new_issues_streams;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.net.HttpURLConnection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class EventHandler implements RequestHandler<Map<String,Object>,Object> {
 
+    private final MongoDb mongoDb;
+    private final String dataFieldName;
     public EventHandler() {
+        mongoDb = new MongoDb();
+        dataFieldName = System.getenv("data_field_name");
     }
 
     public Object handleRequest(Map<String, Object> inputEvent, Context context) {
         context.getLogger().log("inputEvent = " + inputEvent+"\n");
+        Map<?,?> fullDoc = (LinkedHashMap<?,?>)inputEvent.get(dataFieldName);
 
-        Map<?,?> detail = (LinkedHashMap<?, ?>) inputEvent.get("detail");
-        context.getLogger().log("detail.operationType : " + detail.get("operationType") + "\n");
-
-        Map<?,?> fullDoc = (LinkedHashMap<?,?>)detail.get("fullDocument");
         Issue issue = new Issue();
         issue.set_id((String)fullDoc.get("_id"));
         issue.setPostedOn((Long)fullDoc.get("posted_on"));
@@ -26,7 +28,9 @@ public class EventHandler implements RequestHandler<Map<String,Object>,Object> {
         issue.setSubject((String)fullDoc.get("subject"));
         issue.setDescription((String)fullDoc.get("description"));
 
-        context.getLogger().log("issue ==> " + issue + "\n");
-        return null;
+        if(mongoDb.addData(issue))
+            return HttpURLConnection.HTTP_OK;
+
+        return HttpURLConnection.HTTP_UNAVAILABLE;
     }
 }
---
+++ src/main/java/helpdesk_ticketing_system/handle_new_issues_streams/MongoDb.java
@@ -0,0 +1,39 @@
+package helpdesk_ticketing_system.handle_new_issues_streams;
+
+import com.google.gson.Gson;
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+
+public class MongoDb {
+    private final MongoCollection<Document> mongoCollection;
+    private final Gson gson;
+
+    public MongoDb() {
+        String connectionUri = System.getenv("mongodb_connection_uri");
+        String username = System.getenv("mongodb_username");
+        String password = System.getenv("mongodb_password");
+        String database = System.getenv("mongodb_database");
+        String collection = System.getenv("mongodb_collection");
+
+        System.out.println("connectionUri = " + connectionUri);
+        String connectionString = String.format(connectionUri, username, password);
+        MongoClient mongoClient = MongoClients.create(
+                MongoClientSettings.builder()
+                        .serverApi(ServerApi.builder().version(ServerApiVersion.V1).build())
+                        .applyConnectionString(new ConnectionString(connectionString))
+                        .build()
+        );
+
+        mongoCollection = mongoClient.getDatabase(database).getCollection(collection);
+        gson = new Gson();
+    }
+    boolean addData(Issue issue)
+    {
+        InsertOneResult result = mongoCollection.insertOne(Document.parse(gson.toJson(issue)));
+        return result.wasAcknowledged();
+    }
+}
